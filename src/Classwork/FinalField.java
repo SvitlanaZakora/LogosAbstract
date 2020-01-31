8c4c5f59f161e21dbb8f544d948f80c4e12ef626
PavlenkoS <pavlenko.sasha@ukr.net>
2020-01-31 15:44:41 +0200

    Adding classwork and homework

---
+++ src/Classwork/FinalField.java
@@ -0,0 +1,11 @@
+package Classwork;
+
+public class FinalField {
+    public String name;
+    public final String field="fff";
+    public void f(){
+//        this.field="jhghf"; not becouse final
+//        MY_CONSTANT="hjkudihgsv"; not be becouse static
+    }
+    public static final String MY_CONSTANT = "Iam constant";
+}
---
+++ src/Classwork/SabClassFromFinal.java
@@ -0,0 +1,13 @@
+package Classwork;
+
+public class SabClassFromFinal extends FinalMethod {
+//    can not be overide
+//    public void f(){
+//        System.out.println("I am from sub class");
+//    }
+
+    @Override
+    public void g(){
+        super.g();
+    }
+}
